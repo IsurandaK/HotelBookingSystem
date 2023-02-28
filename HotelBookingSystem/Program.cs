6c5e0375850eb1269ab755a5114babbe47c26155
koshi <koshi@KOSHILA>
2023-02-28 21:57:50 +0530

    Add project files.

---
+++ HotelBookingSystem/Program.cs
@@ -0,0 +1,37 @@
+
+using HotelBookingSystem.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<hotelbookingsystemContext>(
+    options =>
+    {
+        options.UseMySql(builder.Configuration.GetConnectionString("hotelbookingsystem"),
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31"));
+    }
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
